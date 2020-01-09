3d0e98a1fb7afc1493e4feeed2ec1934ca521c8c
sasa02sasa <sasa02sasa@mail.ru>
2020-01-09 12:43:37 +0600

    The App component has built-in authorization through social networks сomponent (template). Add animation of changing authorization at registration components.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,11 +3,12 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Login from '../login';
+import AuthSocial from '../authSocial';
 import Register from '../register';
 import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core/styles';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 const useStyles = makeStyles({
@@ -22,7 +23,8 @@ const useStyles = makeStyles({
     height: '100%'
   },
   fragment: {
-    width: '50%'
+    width: '50%',
+    height: '100%'
   },
   arrow: {
     transition: 'transform .2s ease-in-out'
@@ -39,31 +41,84 @@ const useStyles = makeStyles({
   content: {
     padding: 15,
     height: '100%'
+  },
+  height: {
+    height: '100%'
   }
 });
 
 const App = () => {
-  const { card, fragment, arrow, deg180, deg0, media, cardContent, content } = useStyles();
-  const [showAuth, setShowAuth] = useState(true);
+  const { card, fragment, arrow, deg180, deg0, media, cardContent, content, height } = useStyles();
+  const [showAuth, setShowAuth] = useState(false);
+  const [display, setDisplay] = useState('block');
+  const [opacity, setOpacity] = useState(1);
+  const [displayElem, setDisplayElem] = useState('none');
+  const [opacityElem, setOpacityElem] = useState(0);
+  const myRef = React.createRef();
+  const myRefElem = React.createRef();
+
   const changeForm = () => {
     setShowAuth(!showAuth);
+    if (display == 'block') {
+      setOpacity(0);
+      myRef.current.addEventListener('transitionend', () => {
+        setDisplay('none');
+        setDisplayElem('block');
+        setOpacityElem(0);
+        setTimeout(() => {
+          setOpacityElem(1);
+        }, 0);
+      }, { once: true });
+    }
+    else if (display == 'none') {
+      setOpacityElem(0);
+      myRefElem.current.addEventListener('transitionend', () => {
+        setDisplayElem('none');
+        setDisplay('block');
+        setOpacity(0);
+        setTimeout(() => {
+          setOpacity(1);
+        }, 0);
+      }, { once: true });
+    }
   };
-  let form = showAuth ? <Login /> : <Register />;
+
   return (
-    <Grid container justify="center" alignItems="center" style={{ height: '100%' }}>
-      <Card className={card}>
+    <Grid container justify="center" alignItems="center" className={height}>
+      <Card className={card} variant="outlined">
         <CardContent className={cardContent}>
-          <Grid container direction="row" style={{ height: '100%' }} justify="space-between">
+          <Grid container direction="row-reverse" className={height} justify="space-between">
             <div className={fragment}>
               <Grid container direction="column" className={content} justify="space-between">
                 <Grid container direction="row" justify="flex-end">
                   <IconButton onClick={changeForm} size="small">
-                    <ArrowForwardIcon fontSize="medium" className={arrow + ' ' + (showAuth ? deg0 : deg180)}></ArrowForwardIcon>
+                    <ArrowForwardIcon className={arrow + ' ' + (showAuth ? deg180 : deg0)}></ArrowForwardIcon>
                   </IconButton>
                 </Grid>
-                <Grid container direction="row" justify="space-between" style={{ flexGrow: 1 }}  >
-                  {form}
+                <Grid container direction="row" justify="space-between" style={{ flexGrow: 1 }}>
+                  <div
+                    style={{
+                      transition: 'all .5s linear',
+                      display: display,
+                      opacity: opacity,
+                      width: '100%'
+                    }}
+                    ref={myRef}
+                  >
+                    <Login></Login>
+                  </div>
+                  <div
+                    style={{
+                      transition: 'all .5s linear',
+                      display: displayElem,
+                      opacity: opacityElem,
+                    }}
+                    ref={myRefElem}
+                  >
+                    <Register></Register>
+                  </div>
                 </Grid>
+                <AuthSocial />
               </Grid>
             </div>
             <div className={fragment}>
