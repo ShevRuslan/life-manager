13e5d7a8aee7456dfb6c0fb9e357e40a26ab1eca
sasa02sasa <sasa02sasa@mail.ru>
2020-01-20 00:07:03 +0600

    Separation of a component Register into UI, Logic, Export

---
+++ src/components/Register/RegisterLogic.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import useForm from '../useForm/useForm';
+import LifeManagerApiService from '../../services';
+import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+
+const validate = values => {
+  let errors = {};
+  if (!values.email) {
+    errors.email = 'Введите email адрес.';
+  } else if (
+    // eslint-disable-next-line no-useless-escape
+    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      values.email
+    )
+  ) {
+    errors.email = 'Неправильно набран email адрес.';
+  }
+  if (!values.password) {
+    errors.password = 'Введите пароль.';
+  } else if (values.password.length < 8) {
+    errors.password = 'Пароль должен быть больше 8 символов.';
+  }
+
+  if (!values.name) errors.name = 'Введите имя!';
+
+  if (!values.surname) errors.surname = 'Введите фамилию!';
+
+  return errors;
+};
+
+const hocRegister = View => props => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [open, setOpen] = useState(false); //Флаг, отвечающий за показ Snackbar
+  const { values, errors, handleChange, handleSubmit } = useForm(reg, validate);
+
+  //FIXME:Добавлять в redux-state
+  async function reg() {
+    const api = new LifeManagerApiService();
+    const data = JSON.stringify(values);
+    const res = await api.registerUser(data);
+    console.log(res);
+  }
+
+  const openViewError = useCallback(() => {
+    if (Object.keys(errors).length === 0) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  });
+
+  useEffect(() => {
+    openViewError();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errors]);
+
+  const handleClose = (e, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const refHandleChange = e => {
+    handleChange(e);
+    openViewError();
+  };
+
+  const viewErrors = () => {
+    let viewError = null;
+    if (Object.keys(errors).length !== 0) {
+      viewError = (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          {
+            <Alert elevation={6} variant="filled" severity="error" onClose={handleClose}>
+              {Object.keys(errors).map((fieldName, key) => {
+                return <div key={key}>{errors[fieldName]}</div>;
+              })}
+            </Alert>
+          }
+        </Snackbar>
+      );
+    }
+    return viewError;
+  };
+
+  return (
+    <View
+      viewErrors={viewErrors}
+      errors={errors}
+      values={values}
+      refHandleChange={refHandleChange}
+      handleClickShowPassword={handleClickShowPassword}
+      showPassword={showPassword}
+      handleSubmit={handleSubmit}
+      {...props}
+    />
+  );
+};
+export default hocRegister;
--- src/components/Register/RegisterUI.jsx
+++ src/components/Register/RegisterUI.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -10,97 +10,10 @@ import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
-import Alert from '@material-ui/lab/Alert';
-import Snackbar from '@material-ui/core/Snackbar';
-import useForm from '../useForm/useForm';
-import LifeManagerApiService from '../../services/';
+import RegisterLogic from './RegisterLogic';
 
-const validate = values => {
-  let errors = {};
-  if (!values.email) {
-    errors.email = 'Введите email адрес.';
-  } else if (
-    // eslint-disable-next-line no-useless-escape
-    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      values.email
-    )
-  ) {
-    errors.email = 'Неправильно набран email адрес.';
-  }
-  if (!values.password) {
-    errors.password = 'Введите пароль.';
-  } else if (values.password.length < 8) {
-    errors.password = 'Пароль должен быть больше 8 символов.';
-  }
-
-  if (!values.name) errors.name = 'Введите имя!';
-
-  if (!values.surname) errors.surname = 'Введите фамилию!';
-
-  return errors;
-};
-
-const Register = () => {
+const RegisterUI = ({viewErrors, errors, values, refHandleChange, handleClickShowPassword, showPassword, handleSubmit }) => {
   const { input, form, typography, adjacentElement, register } = useStyles();
-  const [showPassword, setShowPassword] = useState(false);
-  const [open, setOpen] = useState(false); //Флаг, отвечающий за показ Snackbar
-  const { values, errors, handleChange, handleSubmit } = useForm(reg, validate);
-
-  //FIXME:Добавлять в redux-state
-  async function reg() {
-    const api = new LifeManagerApiService();
-    const data = JSON.stringify(values);
-    const res = await api.registerUser(data);
-    console.log(res);
-  }
-
-  const openViewError = useCallback(() => {
-    if (Object.keys(errors).length === 0) {
-      setOpen(false);
-    } else {
-      setOpen(true);
-    }
-  });
-
-  useEffect(() => {
-    openViewError();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [errors]);
-
-  const handleClose = (e, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const refHandleChange = e => {
-    handleChange(e);
-    openViewError();
-  };
-
-  const viewErrors = () => {
-    let viewError = null;
-    if (Object.keys(errors).length !== 0) {
-      viewError = (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          {
-            <Alert elevation={6} variant="filled" severity="error" onClose={handleClose}>
-              {Object.keys(errors).map((fieldName, key) => {
-                return <div key={key}>{errors[fieldName]}</div>;
-              })}
-            </Alert>
-          }
-        </Snackbar>
-      );
-    }
-    return viewError;
-  };
-
   return (
     <div className={register}>
       {viewErrors()}
@@ -179,4 +92,4 @@ const Register = () => {
     </div>
   );
 };
-export default Register;
+export default RegisterLogic(RegisterUI);
