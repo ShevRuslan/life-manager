969376cede43d46b59fd972e7ae536a4f360fc6d
sasa02sasa <sasa02sasa@mail.ru>
2020-01-25 17:44:33 +0600

    Connect redux store in React Application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/app/App.jsx';
+import {BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+  document.getElementById('root'));
 
