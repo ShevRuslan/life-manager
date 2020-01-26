9f1be3f6ce81f45059198fb2377553bb3d59c97c
sasa02sasa <sasa02sasa@mail.ru>
2020-01-26 20:08:45 +0600

    Create validate rules in routes (login, register)

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const authClass = require('../modules/auth');
-const auth = new authClass();
+const { loginUser, registerUser, refreshToken, private } = new authClass();
 const passport = require('passport');
+const { check } = require('express-validator');
 
-router.post('/login', auth.loginUser)
-router.post('/register', auth.registerUser)
-router.post('/token', auth.refreshToken)
-router.get('/secret', passport.authenticate('jwt', { session: false }), auth.private);
+router.post('/login', [
+  check('email')
+    .exists().withMessage('Введите почту!')
+    .isEmail().withMessage('Вы неправильно ввели почту!'),
+  check('password')
+    .exists().withMessage('Введите пароль!')
+    .isLength({ min: 10 }).withMessage('Пароль должен быть больше 8 символов!')
+], loginUser)
+
+router.post('/register', [
+  check('name').exists().withMessage('Имя не должно быть пустым!'),
+  check('surname').exists().withMessage('Фамилия не должна быть пустой!'),
+  check('email')
+    .exists().withMessage('Введите почту!')
+    .isEmail().withMessage('Вы неправильно ввели почту!'),
+  check('password')
+    .exists().withMessage('Введите пароль!')
+    .isLength({ min: 8 }).withMessage('Пароль должен быть больше 8 символов!')
+] ,registerUser)
+
+router.post('/token', refreshToken)
+
+router.get('/secret', passport.authenticate('jwt', { session: false }), private);
 
 module.exports = router;
