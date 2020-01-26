b227996ee4eeef462f2688a6733d073f38cf1d10
sasa02sasa <sasa02sasa@mail.ru>
2020-01-26 21:12:14 +0600

    Add return erros in back-end

--- server/modules/auth.js
+++ server/modules/auth.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const passportJWT = require('passport-jwt');
 const ExtractJWT = passportJWT.ExtractJwt;
 const JWTStrategy = passportJWT.Strategy;
+const { validationResult } = require('express-validator');
 
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
@@ -27,69 +28,78 @@ passport.use(strategy);
 
 module.exports = class {
   registerUser = async (request, response) => {
+    const errors = validationResult(request).mapped();
+
+    if (errors) {
+      return response.status(422).json({ errors: errors });
+    }
+
+
     const { name, surname, email, password } = request.body;
-    if (name && password && surname && email) {
-      const user = await User.findOne({ email: email });
-      if (user) {
-        response.status(401).json({ message: 'Пользователь существует!' });
-      }
-      const countUsers = await User.count({});
-      const lastId = countUsers + 1;
-      const token = jwt.sign({ id: lastId }, config.secret, { expiresIn: config.tokenLife });
-      const refreshToken = jwt.sign({ id: lastId }, config.refreshTokenSecret, {
-        expiresIn: config.refreshTokenLife
+
+    const user = await User.findOne({ email: email });
+    if (user) {
+      response.status(401).json({ message: 'Пользователь существует!' });
+    }
+    const countUsers = await User.count({});
+    const lastId = countUsers + 1;
+    const token = jwt.sign({ id: lastId }, config.secret, { expiresIn: config.tokenLife });
+    const refreshToken = jwt.sign({ id: lastId }, config.refreshTokenSecret, {
+      expiresIn: config.refreshTokenLife
+    });
+    let newUser = new User({
+      name: name,
+      surname: surname,
+      email: email,
+      password: bcrypt.hashSync(password, salt),
+      refresh_token: refreshToken
+    });
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const saveUser = await newUser.save();
+      response.status(200).json({
+        status: 'Регистрация прошла успешно!',
+        success: true,
+        token: token,
+        refreshToken: refreshToken
       });
-      let newUser = new User({
-        name: name,
-        surname: surname,
-        email: email,
-        password: bcrypt.hashSync(password, salt),
-        refresh_token: refreshToken
-      });
-      try {
-        // eslint-disable-next-line no-unused-vars
-        const saveUser = await newUser.save();
-        response.status(200).json({
-          status: 'Регистрация прошла успешно!',
-          success: true,
-          token: token,
-          refreshToken: refreshToken
-        });
-      } catch (err) {
-        response.status(500).json({ message: 'Регистрация провалилась.' });
-      }
+    } catch (err) {
+      response.status(500).json({ message: 'Регистрация провалилась.' });
     }
   };
   loginUser = async (request, response) => {
+    const errors = validationResult(request).mapped();
+
+    if (errors) {
+      return response.status(200).json({ errors: errors });
+    }
+  
     const { email, password } = request.body;
-    if (email && password) {
-      try {
-        const user = await User.findOne({ email });
+    
+    try {
+      const user = await User.findOne({ email });
 
-        if (!user) {
-          response.status(401).json({ message: 'Пользователь не найден.' });
-        }
-
-        if (bcrypt.compareSync(password, user.password)) {
-          const token = jwt.sign({ id: user.id }, config.secret, { expiresIn: config.tokenLife });
-          const refreshToken = jwt.sign({ id: user.id }, config.refreshTokenSecret, {
-            expiresIn: config.refreshTokenLife
-          });
-          user.refresh_token = refreshToken;
-          await user.save();
-          const res = {
-            'status': 'Logged in',
-            'success': true,
-            'token': token,
-            'refreshToken': refreshToken
-          };
-          response.status(200).json(res);
-        }
-      } catch (err) {
-        response.status(401).json({ message: `Ошибка с базой данных. ${err}` });
+      if (!user) {
+        response.status(401).json({ message: 'Пользователь не найден.' });
       }
-    } else {
-      response.status(401).json({ message: 'password not match' });
+
+      if (bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign({ id: user.id }, config.secret, { expiresIn: config.tokenLife });
+        const refreshToken = jwt.sign({ id: user.id }, config.refreshTokenSecret, {
+          expiresIn: config.refreshTokenLife
+        });
+        user.refresh_token = refreshToken;
+        await user.save();
+        const res = {
+          'status': 'Logged in',
+          'success': true,
+          'token': token,
+          'refreshToken': refreshToken
+        };
+        response.status(200).json(res);
+      }
+    } catch (err) {
+      response.status(401).json({ message: `Ошибка с базой данных. ${err}` });
     }
   };
   refreshToken = async (request, response) => {
@@ -121,7 +131,7 @@ module.exports = class {
       response.status(401).json({ message: `${err}` });
     }
   };
-  private = async (request, response) => {
+  private = (request, response) => {
     response.status(200).json({ success: 'is private! token very nice' });
   };
 };
